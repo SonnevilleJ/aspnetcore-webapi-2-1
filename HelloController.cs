7f5f8970415636649b145a085c866973ced91ceb
John Sonneville <SonnevilleJ@gmail.com>
2018-07-11 23:34:15 -0500

    Works on Linux

---
+++ HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi
+{
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return new {a = 5, b = "hi"};
+        }
+    }
+}
